b1d7d4cfa11d24af3514c2ffbd8f3cce37ad00c8
agentwyz <2012663060@qq.com>
2023-04-04 20:04:41 +0800

    init dir

---
+++ dfastate/state.go
@@ -0,0 +1,13 @@
+package dfastate
+
+type State string
+
+
+const (
+	Init = "Init"
+	Id = "Id"
+	GT = ">"
+	GE = ">="
+	IntLiteral = "IntLiteral"
+	Plus = "+"
+)
---
+++ token/tokenType.go
@@ -0,0 +1,17 @@
+package token
+
+type Type string
+
+const (
+	ILLEGAL = "ILLEGAL"
+
+	IDENT = "IDENT"
+	
+	GT = "GT"
+	
+	INT = "INT"
+	
+	GE = "GE"
+	
+	PLUS = "PLUS"
+)
